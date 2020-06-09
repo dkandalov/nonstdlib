714ffdf4564c0ec8bf456fd31faebd33e75d9ee9
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2020-06-09 17:30:40 +0100

    String.nullIfEmpty()

--- src/main/kotlin/nonstdlib/string.kt
+++ src/main/kotlin/nonstdlib/string.kt
@@ -11,8 +11,10 @@ fun String.tail(): String = drop(1)
 
 operator fun String.times(size: Int): String = 0.until(size).joinBy("") { this }
 
-fun Any.formattedAs(formatString: String) = formatString.format(this)
+fun String?.nullIfEmpty(): String? = if (this == null || this.isEmpty()) null else this
 
 fun String.normaliseLineBreaks(): String = replace("\r\n", "\n").replace("\r", "\n")
 
 fun String.toInputStream(charset: Charset = UTF_8): InputStream = ByteArrayInputStream(this.toByteArray(charset))
+
+fun Any.formattedAs(formatString: String) = formatString.format(this)
--- src/test/kotlin/nonstdlib/StringTests.kt
+++ src/test/kotlin/nonstdlib/StringTests.kt
@@ -6,13 +6,24 @@ import kotlin.text.Charsets.UTF_8
 
 class StringTests {
     @Test fun `it works`() {
+        "".size shouldEqual 0
         "abcde".size shouldEqual 5
+
+        "".tail() shouldEqual ""
         "abcde".tail() shouldEqual "bcde"
+
         "abcde" * 0 shouldEqual ""
         "abcde" * 1 shouldEqual "abcde"
         "abcde" * 2 shouldEqual "abcdeabcde"
+
+        "".nullIfEmpty() shouldEqual null
+        "abcde".nullIfEmpty() shouldEqual "abcde"
+
         42.formattedAs("%04d") shouldEqual "0042"
+
+        "\n".normaliseLineBreaks() shouldEqual "\n"
         "foo\r\nbar\rbuzz\n".normaliseLineBreaks() shouldEqual "foo\nbar\nbuzz\n"
+
         "abcde".toInputStream().readAllBytes().toString(UTF_8) shouldEqual "abcde"
     }
 }
